d69decd551ececbe32341567674b3bcf105ffa97
Carla Sánchez <carmsanc@espol.edu.ec>
2021-08-15 15:41:44 -0500

    Guard para acceso a páginas que requieren autenticación

---
+++ miweb/src/app/guards/authenticated-user.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggedUserGuard implements CanActivate {
+  constructor(
+    private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      const hasAccess = (localStorage.getItem('type') === 'final');
+      const isValidToken = localStorage.getItem('token') != null && localStorage.getItem('token') !== '';
+
+      if (isValidToken && hasAccess){
+        return true;
+      } else {
+        Swal.fire('Acceso denegado', 'Inicie sesión para acceder a está página.', 'error');
+
+        this.router.navigateByUrl('/');
+        return false;
+      }
+  }
+
+}
